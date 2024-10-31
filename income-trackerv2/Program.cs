f32503dc60dfcfe42ce79f32472e6d88f067cbc4
Kociarz <dbednorz@tuta.io>
2024-10-31 15:34:55 +0100

    first commit

---
+++ income-trackerv2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace income_trackerv2;
+
+// TODO: Make class expense create new JSON file whenever an object is created
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isRunning = true;
+
+        while(isRunning){
+            System.Console.WriteLine("Expense name: ");
+            string expenseName = Console.ReadLine();
+
+            System.Console.WriteLine("Expense ammount: ");
+            int expenseAmmount = int.Parse(Console.ReadLine());
+
+            Expense expense = new Expense{
+                _ExpenseName = expenseName,
+                _ExpenseAmmount = expenseAmmount
+            };
+            
+        }
+
+        
+
+    }
+}
+
+class Expense{
+    public string _ExpenseName {get; set;}
+    public int _ExpenseAmmount {get; set;}
+
+}
